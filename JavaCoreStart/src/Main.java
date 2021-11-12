12412e1febdd870047d7398072bfeb22b66ae13d
Th3Ro <nosolencko@gmail.com>
2021-11-12 12:54:55 +0300

    Second Task
    Variables of all types have been created and initialized.

--- JavaCoreStart/src/Main.java
+++ JavaCoreStart/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+        byte byteVariable = 127;
+        short shortVariable = 32767;
+        int intVariable = -2147483648;
+        long longVariable = 100L;
+        float floatVariable = 65.546f;
+        double doubleVariable = 12.04534;
+        char charVariable = 'a';
+        boolean booleanVariable = false;
     }
 }
