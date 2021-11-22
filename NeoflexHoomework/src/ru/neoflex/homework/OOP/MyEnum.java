e02ed1d178e9506fd94dde110938b72e80821a9f
Th3Ro <nosolencko@gmail.com>
2021-11-22 13:27:15 +0300

    Chande task 3
    Array "Values" replaced with ArrtayList

--- NeoflexHoomework/src/ru/neoflex/homework/OOP/MyEnum.java
+++ NeoflexHoomework/src/ru/neoflex/homework/OOP/MyEnum.java
@@ -1,11 +1,16 @@
 package NeoflexHoomework.src.ru.neoflex.homework.OOP;
 
+import java.util.ArrayList;
+
 public class MyEnum {
     private String name;
 
     private MyEnum (String name) {
         this.name = name;
+        values.add(this);
     }
+
+    public static final ArrayList<MyEnum> values = new ArrayList<>();
 
     public static final MyEnum MONDAY = new MyEnum("monday");
     public static final MyEnum TUESDAY = new MyEnum("tuesday");
@@ -14,8 +19,6 @@ public class MyEnum {
     public static final MyEnum FRIDAY = new MyEnum("friday");
     public static final MyEnum SATURDAY = new MyEnum("saturday");
     public static final MyEnum SUNDAY = new MyEnum("sunday");
-
-    public static final MyEnum[] values = {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY};
 
     public static MyEnum valueOf(String name) {
         for (MyEnum myEnum: values){
